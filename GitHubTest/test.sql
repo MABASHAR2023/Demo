6a74739dfc39be03cc6a127c7dc6e29004136195
MABASHAR2023 <bashar.muhammad@hotmail.com>
2023-10-04 23:15:53 +0100

    Pivot sql
    demo pivot scripts

---
+++ GitHubTest/test.sql
@@ -0,0 +1,52 @@
+
+
+
+--First, select category name and product id from the production.products and production.categories tables as the base data for pivoting:
+
+SELECT 
+    category_name, 
+    COUNT(product_id) product_count
+FROM 
+    production.products p
+    INNER JOIN production.categories c 
+        ON c.category_id = p.category_id
+GROUP BY 
+    category_name
+
+--Second, create a temporary result set using a derived table:
+
+SELECT * FROM (
+    SELECT 
+        category_name, 
+        product_id
+    FROM 
+        production.products p
+        INNER JOIN production.categories c 
+            ON c.category_id = p.category_id
+) t
+
+
+--Third, apply the PIVOT operator:
+
+SELECT * FROM   
+(
+    SELECT 
+        category_name, 
+        product_id,
+		model_year
+    FROM 
+        production.products p
+        INNER JOIN production.categories c 
+            ON c.category_id = p.category_id
+) t 
+PIVOT(
+    COUNT(product_id) 
+    FOR category_name IN (
+        [Children Bicycles], 
+        [Comfort Bicycles], 
+        [Cruisers Bicycles], 
+        [Cyclocross Bicycles], 
+        [Electric Bikes], 
+        [Mountain Bikes], 
+        [Road Bikes])
+) AS pivot_table;
